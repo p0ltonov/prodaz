1372f23322338493cd26aec2193f741722150a82
p0ltonov <139366263+p0ltonov@users.noreply.github.com>
2023-09-13 12:21:53 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,18 +1,19 @@
-import pulp
-import time
-start = time.time()
-x1 = pulp.LpVariable("x1", lowBound=0)
-x2 = pulp.LpVariable("x2", lowBound=0)
-problem = pulp.LpProblem('0',pulp.LpMaximize)
-problem += 30*x1 +x2, "Функция цели"
-problem += 90*x1+ 5*x2 <= 10000, "1"
-problem +=x2 ==3*x1, "2"
-problem.solve()
-print ("Результат:")
-for variable in problem.variables():
- print (variable.name, "=", variable.varValue)
-print ("Прибыль:")
-print (pulp.value(problem.objective))
-stop = time.time()
-print ("Время :")
-print(stop- start)
+import matplotlib.pyplot as plt
+import numpy as np
+plt.plot([95,0],[0,290])
+plt.scatter(58,111)
+plt.text(2500,100,"90x+5y<=10000")
+plt.axhline(y=111,c="green")
+plt.xlim(0,300) #устанавливаем лимиты осей
+plt.ylim(0,300)
+plt.ylabel('y')
+plt.xlabel('x')
+plt.grid() # включение отображение сетки
+plt.title('Продажи')
+plt.yticks(range(0, 300, 20))
+plt.xticks(range(0, 300, 20))
+#plt.fill_between([0,14],[17,17],color="silver")
+#plt.fill_between([13.8,19],[17,13],color="silver")
+
+plt.arrow(1,1,30,30, width=1.2)
+plt.show()
